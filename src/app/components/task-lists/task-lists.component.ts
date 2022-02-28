551f85330ea0cf88b015e4f404f12358ae7cb1d5
derallsm <derrick0393@gmail.com>
2022-02-28 12:27:44 -0600

    Delete task logic added

--- src/app/components/task-lists/task-lists.component.ts
+++ src/app/components/task-lists/task-lists.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MainTask } from 'src/app/models/task.model';
 import { TaskService } from 'src/app/services/task.service';
+
 
 @Component({
   selector: 'app-task-lists',
   templateUrl: './task-lists.component.html',
-  styleUrls: ['./task-lists.component.css']
+  styleUrls: ['./task-lists.component.scss']
 })
 export class TaskListsComponent implements OnInit {
 
   taskLists: MainTask[] = [];
+  private itemToDelete: MainTask;
 
-  constructor( private taskService: TaskService ) {
+  constructor( private taskService: TaskService, private modal: NgbModal ) {
    }
 
   ngOnInit( ) {
     this.taskLists = this.taskService.getTasks();
-    console.log(this.taskLists);
   }
 
-  deleteTask(item: MainTask) {
-    //Show validation
-    let index: number = this.taskLists.indexOf(item);
-    this.taskLists.splice(index, 1);
+  deleteTask(item: MainTask, content) {
+    this.modal.open(content);
+    this.itemToDelete = item;
+  }
+
+  // editTask(item: MainTask) {
+  //   // console.log('inside the edit function');
+  //   // this.taskService.updateTask(item);
+  // }
+
+  close() {
+    this.modal.dismissAll();
+  }
+
+  confirmDelete() {
+    this.taskLists = this.taskService.deleteTask(this.itemToDelete);
+    this.modal.dismissAll();
   }
 
 }
